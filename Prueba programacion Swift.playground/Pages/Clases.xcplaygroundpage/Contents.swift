1fb70e2840ad25ff8372e94cd1a19e8e6e2943f6
josores2 <josesorliquart@gmail.com>
2024-03-26 17:37:57 +0100

    mi primer commit

--- Prueba programacion Swift.playground/Pages/Clases.xcplaygroundpage/Contents.swift
+++ Prueba programacion Swift.playground/Pages/Clases.xcplaygroundpage/Contents.swift
@@ -2,6 +2,43 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+class Programador {
+    let nombre:String
+    let edad: Int
+    let lenguajes : [Lenguaje]
+    var amigos : [Programador]?
+    
+    enum Lenguaje{
+        case swift
+        case kotlin
+        case java
+        case javascript
+    }
+    
+    //Bloque de inicialización
+    init(nombre:String, edad:Int, lenguajes:[Lenguaje], amigos:[Programador]?){
+        self.nombre = nombre
+        self.edad = edad
+        self.lenguajes = lenguajes
+        self.amigos = amigos
+    }
+    
+    func programo(){
+        print("Programo en estos lenguajes")
+    }
+}
 
-//: [Next](@next)
+//Instanciar un objeto de esta clase Programador
+let juan = Programador(nombre: "Juan Muñoz", edad: 26, lenguajes: [.swift,.kotlin],amigos:nil)
+juan.programo()
+
+let zaafir = Programador(nombre: "Zaafir", edad: 19, lenguajes: [.kotlin,.javascript], amigos: nil)
+let albert = Programador(nombre: "Zaafir", edad: 19, lenguajes: [.kotlin,.javascript], amigos: nil)
+zaafir.programo()
+
+juan.amigos = [zaafir,albert]
+
+
+
+print(juan.amigos?.first?.nombre ?? "")
+
